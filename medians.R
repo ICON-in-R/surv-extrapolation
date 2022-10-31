6b3ef4e6f3a933b911a9f31611e0390b22a06369
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-10-31 16:09:51 +0000

    medians

--- medians.R
+++ medians.R
@@ -4,13 +4,14 @@ library(survHE)
 data(bc)
 
 # Fits the same model using the 3 inference methods
-mle <- fit.models(formula = Surv(recyrs,censrec) ~ 1,
+mle <- fit.models(formula = Surv(recyrs, censrec) ~ 1,
                   data = bc,
                   distr = "exp",
                   method = "mle")
+
+# the print and summary methods for a survHE object both return medians
 mle
 
-# includes median
 summary(mle)
 
 
@@ -22,30 +23,32 @@ min(mle$misc$km$time[mle$misc$km$surv < 0.5])
 
 xx <- make.surv(mle)
 
-purrr::map(xx$S, ~ min(.x$t[.x$S < 0.5]))
+purrr::map_dbl(xx$S, ~ min(.x$t[.x$S < 0.5]))
 
 rate <- mle$models$Exponential$coefficients
 
--log(0.5)/exp(rate)
+# closed form
+meantime <- -log(0.5)/exp(rate)
 
 
 # multiple simulations
 
-xx <- make.surv(mle, nsim = 10)
+xx <- make.surv(mle, nsim = 100)
 
-purrr::map(xx$S, ~ min(.x$t[.x$S < 0.5]))
-purrr::map(xx$S, ~ min(.x$t[.x$low < 0.5]))
-purrr::map(xx$S, ~ min(.x$t[.x$upp < 0.5]))
+purrr::map_dbl(xx$S, ~ min(.x$t[.x$S < 0.5]))
+purrr::map_dbl(xx$S, ~ min(.x$t[.x$low < 0.5]))
+purrr::map_dbl(xx$S, ~ min(.x$t[.x$upp < 0.5]))
 
 
-##TODO: where are the stan data for rate?
+rtimes <- purrr::map_dbl(unlist(xx$sim), ~ -log(0.5)/.x)
 
--log(0.5)/exp(rate)
 
+plot(mle) + 
+  geom_vline(xintercept = rtimes, alpha = 0.1, col = "darkgrey", size = 3) +
+  geom_vline(xintercept = meantime)
 
 
 # multiple groups -----------------------
-
 
 # Fits the same model using the 3 inference methods
 mle <- fit.models(formula = Surv(recyrs,censrec) ~ group,
@@ -56,11 +59,11 @@ mle
 
 xx <- make.surv(mle)
 
-purrr::map(xx$S, ~ min(.x$t[.x$S < 0.5]))
+purrr::map_dbl(xx$S, ~ min(.x$t[.x$S < 0.5]))
 
 rate <- mle$models$Exponential$coefficients
 
--log(0.5)/rate
+meantime <- -log(0.5)/rate
 
 
 ## plots
